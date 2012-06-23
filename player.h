aec097159f70aea0b3a38d24fa44dff92482577d
manue1 <mne@manubuntu.(none)>
2012-06-23 12:53:56 +0200

    Moved public variables to private.

--- player.h
+++ player.h
@@ -1,15 +1,15 @@
-#ifndef _PLAYER_H_
-#define _PLAYER_H_
+#ifndef PLAYER_H_
+#define PLAYER_H_
 
 class CPlayer
 {
 private:
 	int code[4];
+	int attempt[4];
 public:
 	friend class CProcessing;
-	int attempt[4];
 	void set_code_generator();
-	void set_code(int &ccounter,char &ccache);
-	void set_attempt(int &ccounter2,int &choice,char &ccache);
+	void set_code(int &ccounter, char &ccache);
+	void set_attempt(int &ccounter2, int &choice, char &ccache);
 };
 #endif
