d742e5ebe6ce7a9f1f31292ea45b062e2b03d51f
manue1 <mne@manubuntu.(none)>
2012-06-26 17:12:57 +0200

    Added comments.

--- rangeexception.h
+++ rangeexception.h
@@ -1,10 +1,14 @@
-#ifndef _RANGEEXCEPTION_H_
-#define _RANGEEXCEPTION_H_
+#ifndef RANGEEXCEPTION_H_
+#define RANGEEXCEPTION_H_
+
+/* CRangeException Klasse:
+ * Eine Exception-Klasse die eine Fehlermeldung ausgibt, wenn ein 'throw' auftritt!
+ */
 
 class CRangeException
 {
 public:
-	string what() {return "Wrong input, please enter only 4 digits between 1-5 and one at a time!\n\n";}
+	string what(){return "Wrong input, please enter only 4 digits between 1-5 and just one at a time!\n\n";}
 };
 
 #endif
