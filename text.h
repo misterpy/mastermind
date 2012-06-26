367be46079928eab6afc0c36c2ae71c3bb7fba94
manue1 <mne@manubuntu.(none)>
2012-06-26 17:14:41 +0200

    Added comments.

--- text.h
+++ text.h
@@ -1,12 +1,16 @@
 #ifndef TEXT_H_
 #define TEXT_H_
 
+/* CText Klasse:
+ * Diese Klasse gibt unterschiedliche Texte waehrend des Spielablaufs aus.
+ */
+
 class CText
 {
 public:
-	string welcome_text();
-	string info_text();
-	string singleplayer();
-	string multiplayer();
+	string welcome_text();		//Funktion zur Ausgabe des Willkommenstexts beim Start des Programms
+	string info_text();			//Funktion zur Ausgabe der Spielinformationen und -regeln
+	string singleplayer();		//Funktion zur Ausgabe des Informationstextes zum Singleplayer-Mode
+	string multiplayer();		//Funktion zur Ausgabe des Informationstextes zum Multiplayer-Mode
 };
 #endif
